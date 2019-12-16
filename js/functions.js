f011a647b4bcb9097fba7925caeba960a607f829
Dovydas Gražys <57098864+notY0U@users.noreply.github.com>
2019-12-16 20:03:54 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -184,79 +184,54 @@ let HTML = '';
     return document.querySelector('#p_img').innerHTML = HTML;
 }
 
-// function renderTestimonials (list)  {
-//     let HTML = '';
-//     let listHTML = '';
-
-//     for ( let i=0; i<list.length; i++ ) {
-//         const testi = Math.floor(Math.random() * list.length);
-//         listHTML +=  `<div class="testimonial ${i===testi ? 'show' : ''}" data-index="${i}">
-//                             <img src="../img/testimonial/${testi.photo}">
-//                                     <h4 class="title">${testi.title}</h4>
-//                             <div class="icon">
-//                                 <i class="fa fa-star"></i>
-//                                 <i class="fa fa-star"></i>
-//                                 <i class="fa fa-star"></i>
-//                                 <i class="fa fa-star"></i>
-//                             </div>
-//                             <div class="description">${testi.description}
-//                             </div> 
-//                     </div>`;
-//     }
-
-//     HTML += `<div class="testimonials">
-//                     <div class="list">
-//                         ${listHTML}
-//                     </div>
-//                     <div class="btns">
-//                     <i class="fa fa-angle-left"></i>
-//                     <div class="circles-btn"> </div>
-//                     <i class="fa fa-angle-right"></i>
-//                 </div>
-//             </div>`;
-                
-//                                                                 //  reikia sukelti i DOM'a
-//      document.querySelector('#testimonial').innerHTML = HTML;
-
-//      const DOMtestimonials = document.querySelector(`.testimonials`)
-//      const visible = DOMtestimonials.querySelector('.testimonial.show');
-//      console.log(DOMtestimonials);
-     
-//      const index = visible.dataset.index;
-//      visible.classList.remove('show');
-
-
-
-//      return;
-
-// }
 // testimonials
-
 function renderClient ( testimonialsList ) {
     let HTML = '';
     let listHTML = '';
     
     for ( let i=0; i<testimonialsList.length; i++ ) {
-        const client = testimonials[i];
-
-        HTML += `<div class="client col-md-12 col-lg-6 col-xs-12">
-                    <div class="icon">
-                        <i class="fa fa-${client.icon}"></i>
-                        <i class="fa fa-${client.icon}"></i>
-                        <i class="fa fa-${client.icon}"></i>
-                        <i class="fa fa-${client.icon}"></i>
-                        <i class="fa fa-${client.icon}"></i>
-                    </div>
-                    <img src="./img/testimonial/${client.photo}">
-            <h4 class="title">${client.title}</h4>
-            <p class="description">${client.description}<p>
-               
-        </div>`
+        const client = testimonials[Math.floor(Math.random() * testimonialsList.length)];
+        listHTML +=  `<div class="testimonial ${i===client ? 'show' : ''}" data-index="${i}">
+                            <img src="./img/testimonial/${client.photo}">
+                                    <h4 class="title">${client.title}</h4>
+                            <div class="icon">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                            <div class="description">${client.description}
+                            </div> 
+                    </div>`;
     }
-    // console.log(testimonials);
-    
-    return document.querySelector('#testimonial').innerHTML = HTML;
+
+    HTML += `<div class="testimonials">
+                    <div class="list">
+                        ${listHTML}
+                    </div>
+                    <div class="btns">
+                    <i class="fa fa-angle-left"></i>
+                    <div class="circles-btn"> </div>
+                    <i class="fa fa-angle-right"></i>
+                </div>
+            </div>`;
+                
+                                                                //  reikia sukelti i DOM'a
+     document.querySelector('#testimonial').innerHTML = HTML;
+
+     const DOMtestimonials = document.querySelector(`.testimonials`)
+     const visible = DOMtestimonials.querySelector('.testimonial.show');
+    //  console.log(DOMtestimonials);
+     
+     const index = visible.dataset.index;
+     visible.classList.remove('show');
+
+
+
+     return;
+
 }
+
 
 //Blog
 function renderBlog( blogList ) {
